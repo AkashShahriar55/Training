c0041d66c34069442228da2ac613e2525cd5e744
akash shahriar <akashshahriar55@gmail.com>
2020-04-29 11:50:05 +0600

    29-04-2020
    from 26 to 29

---
+++ BroadcastReceiverPractice/app/src/main/java/com/example/broadcastreceiverpractice/CustomReceiver.java
@@ -0,0 +1,39 @@
+package com.example.broadcastreceiverpractice;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_CUSTOM_BROADCAST = BuildConfig.APPLICATION_ID+".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+
+        // get the intent action from the intent parameter
+        String intentAction = intent.getAction();
+        // switch for different toast message based on action
+        if(intentAction != null){
+            String toastMessage = "unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power Disconnected";
+                    break;
+                //respond to the custom broadcast
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broadcast Received";
+                    break;
+            }
+            //Display the message
+            Toast.makeText(context,toastMessage,Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ BroadcastReceiverPractice/app/src/main/java/com/example/broadcastreceiverpractice/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.broadcastreceiverpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CustomReceiver mReceiver = new CustomReceiver();
+
+    private static final String ACTION_CUSTOM_BROADCAST
+            = BuildConfig.APPLICATION_ID+".ACTION_CUSTOM_BROADCAST";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //it specifies the types of intents a component can receive
+        //it will filter out the intents based on action and category
+        IntentFilter filter = new IntentFilter();
+        //When the system receives an intent as a broadcast
+        //it searches the receivers based on action value
+
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        //register the receiver using MainActivity context so it will able
+        // to receive broadcasts as long as MainActivity is running
+
+        this.registerReceiver(mReceiver,filter);
+
+        //register custom broadcast
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver
+                ,new IntentFilter(ACTION_CUSTOM_BROADCAST));
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        //Unregister the receiver
+        //to save system resources and avoid leaks
+        //dynamic receiver must be unregistered when not needed
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
+
+        //unregister custom broadcast
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+    }
+
+
+    public void sendCustomBroadcast(View view) {
+        //as this broadcast will be used in this app
+        //LocalBroadcastManager will be used to register
+        // for sending broadcasts within the app
+        //local broadcasts keep information more secure
+        //app data isn't shared with other android apps
+
+        //create a custom broadcast intent
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        //send the broadcast using LocalBroadcastManager
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+    }
+}

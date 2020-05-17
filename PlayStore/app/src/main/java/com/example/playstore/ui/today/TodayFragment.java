3681bdf56d09ec0f2b18a81f23eebfcf20392877
akash shahriar <akashshahriar55@gmail.com>
2020-05-17 16:38:20 +0600

    final version

--- PlayStore/app/src/main/java/com/example/playstore/ui/today/TodayFragment.java
+++ PlayStore/app/src/main/java/com/example/playstore/ui/today/TodayFragment.java
@@ -17,11 +17,6 @@ import com.example.playstore.MyReader;
 import com.example.playstore.R;
 import com.example.playstore.TodayCardAdapter;
 import com.example.playstore.TodayData;
-import com.google.android.gms.ads.AdListener;
-import com.google.android.gms.ads.AdLoader;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.formats.NativeAdOptions;
-import com.google.android.gms.ads.formats.UnifiedNativeAd;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -46,26 +41,6 @@ public class TodayFragment extends Fragment {
 
         initializeRecyclerView(view);
 
-        AdLoader adLoader = new AdLoader.Builder(getContext(), "ca-app-pub-3940256099942544/2247696110")
-                .forUnifiedNativeAd(new UnifiedNativeAd.OnUnifiedNativeAdLoadedListener() {
-                    @Override
-                    public void onUnifiedNativeAdLoaded(UnifiedNativeAd unifiedNativeAd) {
-                        // Show the ad.
-                    }
-                })
-                .withAdListener(new AdListener() {
-                    @Override
-                    public void onAdFailedToLoad(int errorCode) {
-                        // Handle the failure by logging, altering the UI, and so on.
-                    }
-                })
-                .withNativeAdOptions(new NativeAdOptions.Builder()
-                        // Methods in the NativeAdOptions.Builder class can be
-                        // used here to specify individual options settings.
-                        .build())
-                .build();
-
-        adLoader.loadAd(new AdRequest.Builder().build());
     }
 
     private void initializeRecyclerView(View view) {
